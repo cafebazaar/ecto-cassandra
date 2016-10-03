9d0ba798279ef34f9a431f8fbf341029129f8a1c
Ali Rajabi <rajabi048@gmail.com>
2016-10-03 18:26:19 +0330

    Refactor testcase

--- test/cassandra_test.exs
+++ test/cassandra_test.exs
@@ -2,74 +2,57 @@ defmodule CassandraTest do
   use ExUnit.Case
   doctest Cassandra
 
-  alias Cassandra.Protocol
+  import Cassandra.Protocol, only: [run: 2, connect: 1]
+
   alias CQL.{Options, Supported, Query, Prepare, Execute, QueryParams}
+  alias CQL.Result.{Void, Rows}
+
+  setup_all do
+    {:ok, %{socket: socket}} = connect([])
+    run socket, "drop keyspace elixir_cql_test;"
+    run socket, """
+      create keyspace elixir_cql_test
+        with replication = {'class':'SimpleStrategy','replication_factor':1};
+    """
+    run socket, "USE elixir_cql_test;"
+    run socket, """
+      create table users (
+        userid uuid,
+        name varchar,
+        age int,
+        address text,
+        joined_at timestamp,
+        PRIMARY KEY (userid, joined_at)
+      );
+    """
+    :ok
+  end
 
   setup do
-    {:ok, %{socket: socket}} = Protocol.connect([])
+    {:ok, %{socket: socket}} = connect([])
+    run socket, "USE elixir_cql_test;"
     {:ok, %{socket: socket}}
   end
 
   test "OPTIONS", %{socket: socket} do
-    assert :ok = Protocol.send_request(%Options{}, socket)
-    assert %Supported{options: options} = Protocol.receive_responce(socket)
+    assert %Supported{options: options} = run socket, %Options{}
     assert ["CQL_VERSION", "COMPRESSION"] = Keyword.keys(options)
   end
 
-  test "QUERY", %{socket: socket} do
-    q = %Query{query: """
-          create keyspace elixir_cql_test
-          with replication = {'class':'SimpleStrategy','replication_factor':1};
-        """}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
-
-    q = %Query{query: "USE elixir_cql_test;"}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
-
-    q = %Query{query: """
-        create table emp (
-          empid int primary key,
-          emp_first varchar,
-          emp_last varchar,
-          emp_dept varchar
-        );
-        """}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
-
-    q = %Query{query: """
-        insert into emp (empid, emp_first, emp_last, emp_dept)
-          values (1,'fred','smith','eng');
-        """}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
-
-    q = %Query{query: "select * from emp;"}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
+  test "INSERT", %{socket: socket} do
+    assert %Void{} = run socket, """
+      insert into users (userid, name, age, address, joined_at)
+        values (uuid(), 'fred smith', 20, 'US', toTimestamp(now()));
+    """
   end
 
-  test "PREPARE", %{socket: socket} do
-    q = %Query{query: "USE elixir_cql_test;"}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
+  test "INSERT SELECT", %{socket: socket} do
+    assert %Void{} = run socket, """
+      insert into users (userid, name, age, address, joined_at)
+        values (uuid(), 'john doe', 20, 'US', toTimestamp(now()));
+    """
 
-    q = %Prepare{query: "insert into emp (empid, emp_first, emp_last, emp_dept)
- values (?, ?, ?, ?);"}
-    assert :ok = Protocol.send_request(q, socket)
-    %{id: id} = IO.inspect(Protocol.receive_responce(socket))
-
-    q = %Execute{id: id, params: %QueryParams{
-      values: %{emp_first: "Asghar", empid: 15, emp_last: "Sadeghi", emp_dept: "back"},
-      consistency: :ONE,
-    }}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect(Protocol.receive_responce(socket))
-
-    q = %Query{query: "select * from emp;"}
-    assert :ok = Protocol.send_request(q, socket)
-    IO.inspect Protocol.receive_responce(socket)
+    rows = run socket, "select * from users;"
+    assert Enum.find(rows, fn map -> map["name"] == "john doe" end)
   end
 end
