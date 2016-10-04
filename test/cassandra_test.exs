e43193b4e1ba87537166a4728d1f76362b76abe5
Ali Rajabi <rajabi048@gmail.com>
2016-10-04 13:28:07 +0330

    Add prepare,execute test

--- test/cassandra_test.exs
+++ test/cassandra_test.exs
@@ -4,8 +4,8 @@ defmodule CassandraTest do
 
   import Cassandra.Protocol, only: [run: 2, connect: 1]
 
-  alias CQL.{Options, Supported, Query, Prepare, Execute, QueryParams}
-  alias CQL.Result.{Void, Rows}
+  alias CQL.{Options, Supported, Prepare, Execute, QueryParams}
+  alias CQL.Result.Void
 
   setup_all do
     {:ok, %{socket: socket}} = connect([])
@@ -53,6 +53,33 @@ defmodule CassandraTest do
     """
 
     rows = run socket, "select * from users;"
+    assert Enum.find(rows, fn map -> map["name"] == "john doe" end)
+  end
+
+  test "PREPARE", %{socket: socket} do
+    %{id: id} = run socket, %Prepare{query: """
+      insert into users (userid, name, age, address, joined_at)
+        values (uuid(), ?, ?, ?, toTimestamp(now()));
+    """}
+
+    assert %Void{} = run socket, %Execute{
+      id: id,
+      params: %QueryParams{
+        values: ["john doe", 27, "UK"],
+        consistency: :ONE,
+      }
+    }
+
+    %{id: id} = run socket, %Prepare{
+      query: "select name,age from users where age=? and address=? ALLOW FILTERING"
+    }
+
+   rows = run socket, %Execute{
+      id: id,
+      params: %QueryParams{
+        values: [27, "UK"],
+      }
+    }
     assert Enum.find(rows, fn map -> map["name"] == "john doe" end)
   end
 end
