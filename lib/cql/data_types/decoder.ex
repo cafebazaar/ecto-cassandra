807261d74532cb35cb6332150a35cd6050e35aa1
Hassan Zamani <hsn.zamani@gmail.com>
2016-10-06 04:53:56 +0330

    Add varint and decimal data types

--- lib/cql/data_types/decoder.ex
+++ lib/cql/data_types/decoder.ex
@@ -11,7 +11,6 @@ defmodule CQL.DataTypes.Decoder do
   def decode(buffer, :blob     ), do: bytes(buffer)
   def decode(buffer, :boolean  ), do: boolean(buffer)
   def decode(buffer, :counter  ), do: long(buffer)
-  def decode(buffer, :decimal  ), do: buffer #TODO
   def decode(buffer, :double   ), do: double(buffer)
   def decode(buffer, :float    ), do: float(buffer)
   def decode(buffer, :int      ), do: int(buffer)
@@ -19,10 +18,23 @@ defmodule CQL.DataTypes.Decoder do
   def decode(buffer, :text     ), do: {buffer, ""}
   def decode(buffer, :uuid     ), do: uuid(buffer)
   def decode(buffer, :varchar  ), do: {buffer, ""}
-  def decode(buffer, :varint   ), do: buffer #TODO
   def decode(buffer, :inet     ), do: buffer #TODO
   def decode(buffer, :date     ), do: buffer #TODO
   def decode(buffer, :time     ), do: long(buffer)
   def decode(buffer, :smallint ), do: short(buffer)
   def decode(buffer, :tinyint  ), do: tinyint(buffer)
+  def decode(buffer, {:decimal, size}), do: decimal(size, buffer)
+  def decode(buffer, {:varint, size}), do: varint(size, buffer)
+
+  def varint(size, buffer) do
+    size = size * 8
+    <<n::signed-integer-size(size), rest::bytes>> = buffer
+    {n, rest}
+  end
+
+  def decimal(size, buffer) do
+    {scale, buffer} = int(buffer)
+    {unscaled, buffer} = varint(size, buffer)
+    {unscaled * :math.pow(10, -scale), buffer}
+  end
 end
--- lib/cql/data_types/encoder.ex
+++ lib/cql/data_types/encoder.ex
@@ -17,7 +17,6 @@ defmodule CQL.DataTypes.Encoder do
   defp parts(:boolean,   true),  do: [int(1), byte(1)]
   defp parts(:boolean,   false), do: [int(1), byte(0)]
   defp parts(:counter,   value), do: [int(8), long(value)]
-  defp parts(:decimal,   value), do: [value] #TODO
   defp parts(:double,    value), do: [int(8), double(value)]
   defp parts(:float,     value), do: [int(4), float(value)]
   defp parts(:int,       value), do: [int(4), int(value)]
@@ -25,10 +24,26 @@ defmodule CQL.DataTypes.Encoder do
   defp parts(:text,      value), do: [long_string(value)]
   defp parts(:uuid,      value), do: [int(16), uuid(value)]
   defp parts(:varchar,   value), do: [long_string(value)]
-  defp parts(:varint,    value), do: [value] #TODO
   defp parts(:inet,      value), do: [value] #TODO
   defp parts(:date,      value), do: [value] #TODO
   defp parts(:time,      value), do: [int(8), long(value)]
   defp parts(:smallint,  value), do: [int(2), short(value)]
   defp parts(:tinyint,   value), do: [int(1), tinyint(value)]
+  defp parts(:varint,    value), do: [varint(value)]
+
+  defp parts(:decimal,   {unscaled, scale}) do
+    [int(scale), varint(unscaled)]
+  end
+
+  defp varint(n) do
+    bytes = int_bytes(n)
+    bits = bytes * 8
+    [int(bytes), <<n::signed-integer-size(bits)>>]
+  end
+
+  defp int_bytes(x, acc \\ 0)
+  defp int_bytes(x, acc) when x >  127 and x <   256, do: acc + 2
+  defp int_bytes(x, acc) when x <= 127 and x >= -128, do: acc + 1
+  defp int_bytes(x, acc) when x < -128 and x >= -256, do: acc + 2
+  defp int_bytes(x, acc), do: int_bytes(Bitwise.bsr(x, 8), acc + 1)
 end
