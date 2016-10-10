13a6201f44790167811db3755b50352b3b064823
Hassan Zamani <hsn.zamani@gmail.com>
2016-10-10 20:38:42 +0330

    Update version from -beta to -pre

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Cassandra.Mixfile do
 
   def project, do: [
     app: :cassandra,
-    version: "0.1.1-beta",
+    version: "0.1.1-pre",
     elixir: "~> 1.3",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
