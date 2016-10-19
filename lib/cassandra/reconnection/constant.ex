c94c4ce16d24f1f72f153c83af4cbaf3fe96d8e5
Hassan Zamani <hsn.zamani@gmail.com>
2016-10-19 19:46:34 +0330

    Add attempts handling to reconnction policies

--- lib/cassandra/reconnection/constant.ex
+++ lib/cassandra/reconnection/constant.ex
@@ -1,25 +1,32 @@
 defmodule Cassandra.Reconnection.Constant do
   defstruct [
     current: nil,
+    attempts: 0,
+
     initial: 500,
     jitter: 0.2,
     max: 12000,
+    max_attempts: 3,
   ]
 
   defimpl Cassandra.Reconnection.Policy do
     def get(cons) do
-      cons.current
+      if cons.attempts < cons.max_attempts do
+        cons.current
+      else
+        :stop
+      end
     end
 
     def next(cons) do
       current = cons.current || cons.initial
       next = current + cons.initial
       noise = (:rand.uniform - 0.5) * cons.jitter * current
-      %{cons | current: round(min(next, cons.max) + noise)}
+      %{cons | attempts: cons.attempts + 1, current: round(min(next, cons.max) + noise)}
     end
 
     def reset(cons) do
-      %{cons | current: nil}
+      %{cons | attempts: 0, current: nil}
     end
   end
 end
--- lib/cassandra/reconnection/exponential.ex
+++ lib/cassandra/reconnection/exponential.ex
@@ -1,26 +1,33 @@
 defmodule Cassandra.Reconnection.Exponential do
   defstruct [
     current: nil,
+    attempts: 0,
+
     initial: 500,
     multiplayer: 1.6,
     jitter: 0.2,
     max: 12000,
+    max_attempts: 3,
   ]
 
   defimpl Cassandra.Reconnection.Policy do
     def get(exp) do
-      exp.current
+      if exp.attempts < exp.max_attempts do
+        exp.current
+      else
+        :stop
+      end
     end
 
     def next(exp) do
       current = exp.current || exp.initial
       next = current * exp.multiplayer
       noise = (:rand.uniform - 0.5) * exp.jitter * current
-      %{exp | current: round(min(next, exp.max) + noise)}
+      %{exp | attempts: exp.attempts + 1, current: round(min(next, exp.max) + noise)}
     end
 
     def reset(exp) do
-      %{exp | current: nil}
+      %{exp | attempts: 0, current: nil}
     end
   end
 end
