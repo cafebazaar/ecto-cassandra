8d872e9c6804af1c94fd9b6eed22d93d7518d8d0
Ali Rajabi <rajabi048@gmail.com>
2016-11-06 16:56:41 +0330

    Add storage tests

---
+++ test/cassandra/ecto/storage_test.exs
@@ -0,0 +1,32 @@
+defmodule EctoStorageTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    options = [
+      keyspace: "test",
+      contact_points: ["127.0.0.1"],
+      replication: [
+        class: "SimpleStrategy",
+        replication_factor: 1,
+      ]
+    ]
+    {:ok, options: options}
+  end
+
+  test "create keyspace", %{options: options} do
+    assert Cassandra.Ecto.create_keyspace(options) == join """
+        CREATE KEYSPACE test
+          WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
+      """
+  end
+
+  test "drop keyspace", %{options: options} do
+    assert Cassandra.Ecto.drop_keyspace(options) == "DROP KEYSPACE test"
+  end
+
+  defp join(str) do
+    str
+    |> String.replace(~r/\s+/, " ")
+    |> String.trim
+  end
+end
