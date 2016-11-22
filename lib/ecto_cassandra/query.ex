c8eed84b752ee7f8322ec5aa216d4a62d6587f94
Hassan Zamani <hsn.zamani@gmail.com>
2016-11-22 20:02:13 +0330

    Accept multiple fields in token function

--- lib/ecto_cassandra/query.ex
+++ lib/ecto_cassandra/query.ex
@@ -30,72 +30,54 @@ defmodule EctoCassandra.Query do
     end
   end
 
+  defmacro token(fields) when is_list(fields) do
+    quote do: fragment("token(?)", unquote_splicing(fields))
+  end
+
   defmacro token(field) do
-    quote do
-      fragment("token(?)", unquote(field))
-    end
+    quote do: fragment("token(?)", unquote(field))
   end
 
   defmacro cast(field, type) when type in @types do
     fragment = "cast(? as #{Atom.to_string(type)})"
-    quote do
-      fragment(unquote(fragment), unquote(field))
-    end
+    quote do: fragment(unquote(fragment), unquote(field))
   end
 
   defmacro uuid do
-    quote do
-      fragment("uuid()")
-    end
+    quote do: fragment("uuid()")
   end
 
   defmacro now do
-    quote do
-      fragment("now()")
-    end
+    quote do: fragment("now()")
   end
 
   defmacro min_timeuuid(time) do
-    quote do
-      fragment("minTimeuuid(?)", unquote(time))
-    end
+    quote do: fragment("minTimeuuid(?)", unquote(time))
   end
 
   defmacro max_timeuuid(time) do
-    quote do
-      fragment("maxTimeuuid(?)", unquote(time))
-    end
+    quote do: fragment("maxTimeuuid(?)", unquote(time))
   end
 
   defmacro to_date(time) do
-    quote do
-      fragment("toDate(?)", unquote(time))
-    end
+    quote do: fragment("toDate(?)", unquote(time))
   end
 
   defmacro to_timestamp(time) do
-    quote do
-      fragment("toTimestamp(?)", unquote(time))
-    end
+    quote do: fragment("toTimestamp(?)", unquote(time))
   end
 
   defmacro to_unix_timestamp(time) do
-    quote do
-      fragment("toUnixTimestamp(?)", unquote(time))
-    end
+    quote do: fragment("toUnixTimestamp(?)", unquote(time))
   end
 
   defmacro as_blob(field, type) when type in @types do
     fragment = "#{Atom.to_string(type)}AsBlob(?)"
-    quote do
-      fragment(unquote(fragment), unquote(field))
-    end
+    quote do: fragment(unquote(fragment), unquote(field))
   end
 
   defmacro contains(field, value) do
-    quote do
-      fragment("? CONTAINS ?", unquote(field), unquote(value))
-    end
+    quote do: fragment("? CONTAINS ?", unquote(field), unquote(value))
   end
 
   @doc """
@@ -104,26 +86,18 @@ defmodule EctoCassandra.Query do
   Refere to http://docs.datastax.com/en/cql/3.1/cql/cql_using/useBatch.html
   """
   defmacro batch(repo, options \\ [], [do: {:__block__, _, statements}]) do
-    quote do
-      EctoCassandra.Batch.batch(unquote(repo), unquote(options), unquote(statements))
-    end
+    quote do: EctoCassandra.Batch.batch(unquote(repo), unquote(options), unquote(statements))
   end
 
   defmacro insert(struct, options \\ []) do
-    quote do
-      {:insert, unquote(struct), unquote(options)}
-    end
+    quote do: {:insert, unquote(struct), unquote(options)}
   end
 
   defmacro update(struct, options \\ []) do
-    quote do
-      {:update, unquote(struct), unquote(options)}
-    end
+    quote do: {:update, unquote(struct), unquote(options)}
   end
 
   defmacro delete(struct, options \\ []) do
-    quote do
-      {:delete, unquote(struct), unquote(options)}
-    end
+    quote do: {:delete, unquote(struct), unquote(options)}
   end
 end
