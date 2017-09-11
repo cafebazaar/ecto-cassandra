9130ba7891ead9432ed3948912508c1c4b95e346
Ali Rajabi <rajabi048@gmail.com>
2017-09-11 15:32:17 +0430

    Update deps

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EctoCassandra.Mixfile do
 
   def project, do: [
     app: :ecto_cassandra,
-    version: "1.0.0-rc.1",
+    version: "1.0.0-rc.2",
     elixir: "~> 1.4",
     build_embedded: Mix.env == :prod,
     start_permanent: Mix.env == :prod,
@@ -30,7 +30,6 @@ defmodule EctoCassandra.Mixfile do
     {:cassandra, "~> 1.0.0-rc.1"},
     {:excoveralls, "~> 0.6", only: :test},
     {:ex_doc, ">= 0.0.0", only: :dev},
-    {:lz4, github: "szktty/erlang-lz4", override: true}, # TODO check if fixed remove
   ]
 
   defp package, do: [
